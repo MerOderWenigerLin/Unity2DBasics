0463cb9ee70ae9d000b9f701379fdd26625d3206
Merlin Bezrodnow <none@null.not>
2018-10-03 19:57:44 +0200

    inital commit - adding basic functionality to git

---
+++ BoxCollider2DHelper.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Unity2DBasics
+{
+    public static class BoxCollider2DHelper
+    {
+        public static Rect toRect(BoxCollider2D boxCollider)
+        {
+            Transform transform = boxCollider.transform;
+            Vector2 position = boxCollider.transform.position;
+            Vector2 scale = boxCollider.transform.localScale;
+            float x;
+            if (transform.localEulerAngles.y == 0)
+                x = position.x + boxCollider.offset.x * scale.x - boxCollider.size.x / 2 * scale.x;
+            else
+                x = position.x - boxCollider.offset.x * scale.x - boxCollider.size.x / 2 * scale.x;
+
+            float y = position.y + boxCollider.offset.y * scale.y - boxCollider.size.y / 2 * scale.y;
+
+            return new Rect(x, y, boxCollider.size.x * scale.x, boxCollider.size.y * scale.y);
+        }
+    }
+}
---
+++ DebugHelper.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Unity2DBasics
+{
+    public static class DebugHelper
+    {
+        public static void drawPoint(Vector2 point, Color color)
+        {
+            Debug.DrawRay(point, Vector2.up, color);
+            Debug.DrawRay(point, Vector2.down, color);
+            Debug.DrawRay(point, Vector2.right, color);
+            Debug.DrawRay(point, Vector2.left, color);
+        }
+
+        public static void drawRectPoints(Rect rect, Color color)
+        {
+            drawPoint(rect.position, color);
+            rect.y = rect.yMax;
+            drawPoint(rect.position, color);
+            rect.x = rect.xMax;
+            drawPoint(rect.position, color);
+            rect.y = rect.y - rect.height;
+            drawPoint(rect.position, color);
+        }
+    }
+}
